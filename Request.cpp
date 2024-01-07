1709663824147c92dfbd151cc6e44421835eca0f
3umi <oumaimalmao23@gmail.com>
2024-01-08 00:25:12 +0100

    Fix request validation logic

--- Request.cpp
+++ Request.cpp
@@ -74,10 +74,15 @@ std::string	Request::get_file_path() const {
 /*	additional functions	*/
 void	Request::is_req_well_formed(void) {
 	// Check if Transfer-Encoding header exist and is different to “chunked”.
+	if (_headers.find("Transfer-Encoding") == _headers.end() && _headers.find("Content-Length") == _headers.end() && _request_line.method == "POST") {
+		_status_code = 400;
+	}
+	if (_headers.find("Content-Type") == _headers.end() && _request_line.method == "POST")
+		_status_code = 400;
 	if (_headers.find("Transfer-Encoding") != _headers.end() && _headers["Transfer-Encoding"] != "chunked")
 		_status_code = 501;
 	// Check if Transfer-Encoding not exist, Content-Length not exist and the method is Post
-	if (_headers.find("Transfer-Encoding") != _headers.end() && _headers.find("Content-Length") != _headers.end() && _request_line.method == "POST")
+	if (_headers.find("Transfer-Encoding") != _headers.end() && _headers.find("Content-Length") != _headers.end())
 		_status_code = 400;
 	// Check the request uri contain a character not allowed.
 	if (_request_line.uri.find_first_not_of("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~:/?#[]@!$&'()*+,;=%") != std::string::npos)
